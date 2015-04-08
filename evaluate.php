2e10564f147a163b6631fce6ed9207865d3fa1fa
kishorekalyan <coe12b021@iiitdm.ac.in>
2015-04-08 23:42:42 +0530

    evaluation

--- evaluate.php
+++ evaluate.php
@@ -1,27 +1,64 @@
 <?php
 session_start();
 require("header.php");
-if($_SESSION['faculty_id'])
+
+if($_SESSION['tab']!=1)
+{
  
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
+ $_SESSION['tab']=1;
+ unset($_SESSION['ques_bank_id']);
+unset($_SESSION['selected_exam']);
+ //echo '<meta http-equiv="refresh" content="0;faculty.php" >';
+}
+
+if($_SESSION['faculty_id'])
+{
 if(isset($_SESSION['course']))
  {
-  $type=$_POST['name'];
-  $test_id_list=array();
-  $i=0;
-  $sql=mysql_query("select test_id from tests where course_id=".$_SESSION['course']." and type=".$_POST['name'].")";
- /*
-  //while($test_id_list[$i]=mysql_fetch_array($sql))
-  		{
-  			$i++;
-  		}
-  var_dump($test_id_list);
- // $test_count=count($test_id_list);
-//size of array 
-  $i=$i-1;
-*/
-  
+
+ 	if(isset($_POST['1']) || isset($_POST['2']) || isset($_POST['3']) || isset($_POST['4']) || isset($_POST['5']))
+    {
+       if(isset($_POST['1']))
+	   {
+		 $check=mysql_num_rows(mysql_query("select exam_activation from test where course_id='$_SESSION[course]' and examtype='1' "));
+          if($check==0)
+          {
+          	echo 'No finished exams to evaluate';
+          }
+          else
+          {
+          	 $i=0;
+          	$ans=mysql_query("select answer from com302_q1,com302_q1_julynov_2015 where que_no=question_no ");
+            while($verify=mysql_fetch_array($ans))
+            {
+            $store[$i]=$verify['answer'];
+            echo $store[$i];
+            $i++;
+            }
+           
+
+
+
+
+          }
+	   }
+    }
+
  }
+
+
+
+
+
+
+}
+
+
+
+
+
+  
+ 
 //////////////////////////////////////////////////////////////////////////////////////////////////
 ?>
 <form method="POST" action="evaluate.php">
